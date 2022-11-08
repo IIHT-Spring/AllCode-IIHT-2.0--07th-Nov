a282f55ea939ae5318ab42195b3e7a06f74560c0
ngactive99 <ngactive99@gmail.com>
2022-11-08 16:10:27 +0530

    aggreation

--- Java-oop-basics/src/com/learn/oop/Employee.java
+++ Java-oop-basics/src/com/learn/oop/Employee.java
@@ -2,23 +2,21 @@ package com.learn.oop;
 
 public class Employee {
 	Integer BaseSalary = 50000; // this as parent class
-	// when the name of two methods are same butt the parameters are different 
+	// when the name of two methods are same butt the parameters are different
 	// we call is Method over loading
-	void TotalSalary (Integer BS, Integer Bonus ) {
-	      Integer total = BS + Bonus;
-	      System.out.println(total);
+
+	void TotalSalary(Integer BS, Integer Bonus) {
+		Integer total = BS + Bonus;
+		System.out.println(total);
 	}
-	
-	void TotalSalary(Integer BS, Integer Bonus , Integer insentive ) {
-		 Integer total = BS + Bonus + insentive;
-		 System.out.println(total);
+
+	void TotalSalary(Integer BS, Integer Bonus, Integer insentive) {
+		Integer total = BS + Bonus + insentive;
+		System.out.println(total);
 	}
-	
+
 	void Working() {
 		System.out.println("Employee is working");
 	}
-	
-	
-
 
 }

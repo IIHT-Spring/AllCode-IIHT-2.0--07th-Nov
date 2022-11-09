01afd90cfbadb14b8cdd62dd11983c0e7f0b45bb
ngactive99 <ngactive99@gmail.com>
2022-11-09 15:28:01 +0530

    established connection and wrote dao

--- StudentManagementApp/src/com/start/Start.java
+++ StudentManagementApp/src/com/start/Start.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import com.start.student.Student;
+import com.start.student.StudentDao;
+
 public class Start {
 
 	public static void main(String[] args) throws IOException {
@@ -18,6 +21,21 @@ public class Start {
 
 			if (choice == 1) {
 				// add student
+				System.out.println("Enter Students fullname: ");
+				String name =br.readLine();
+				
+				System.out.println("Enter Student's phone number: ");
+				String phone = br.readLine();
+				
+				System.out.println("Enter Student's city: ");
+				String city = br.readLine();
+				
+				Student st = new  Student(name,phone,city);
+				
+				System.out.println(st);
+				StudentDao.addStudentToDB(st);
+				
+				
 			} else if (choice == 2) {
 				// delete student
 			} else if (choice == 3) {
